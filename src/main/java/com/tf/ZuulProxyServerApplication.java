f9706541f215825ba452a251913aeafe28bc3fbd
pduseja <dusejapt@gmail.com>
2017-04-20 13:16:39 +0530

    Add files via upload

--- src/main/java/com/tf/ZuulProxyServerApplication.java
+++ src/main/java/com/tf/ZuulProxyServerApplication.java
@@ -2,8 +2,10 @@ package com.tf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulProxyServerApplication {
